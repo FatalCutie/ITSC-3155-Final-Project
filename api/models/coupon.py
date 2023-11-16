f567be4745915b2f0b13e49077b6457d6ac72fed
FatalCutie <jcfrosh360@gmail.com>
2023-11-16 16:13:59 -0500

    Added relationship between coupon and recipes
    Im losing my mind

--- api/models/coupon.py
+++ api/models/coupon.py
@@ -11,4 +11,4 @@ class Coupon(Base):
     discount = Column(Integer, index=True, nullable=False, server_default='0.0')
 
     # TODO: These models/schemas haven't been implemented yet, so this doesn't work yet
-    # recipes = relationship("Recipe", back_populates="resource") #recipe where price is stored
+    recipes = relationship("Recipe", back_populates="resource") #recipe where price is stored
