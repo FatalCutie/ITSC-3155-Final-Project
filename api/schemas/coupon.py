f1f73ada125466a03384140552c09952ebf055a4
Alex McComas <alex.mccomas4@yahoo.com>
2023-11-30 16:28:28 -0500

    Updated coupon schema

--- api/schemas/coupon.py
+++ api/schemas/coupon.py
@@ -1,18 +1,27 @@
 from typing import Optional
 from pydantic import BaseModel
 
+
 class CouponBase(BaseModel):
-    discount:int
+    discount: int
+    code: int
+    expiration_date: str
+
 
 class CouponCreate(CouponBase):
     pass
 
+
 class CouponUpdate(BaseModel):
     discount: Optional[int] = None
+    code: Optional[int] = None
+    expiration_date: Optional[str] = None
 
 
 class Coupon(CouponBase):
     discount: int
+    code: int
+    expiration_date = str
 
     class ConfigDict:
         from_attributes = True
