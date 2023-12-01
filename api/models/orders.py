1fa8226cadb7e91c5efe3a56e30672ac69f5fcbf
tatataToua123 <tlee97@uncc.edu>
2023-11-30 19:04:28 -0500

    changed back to original
    I changed things back to the original because I was getting errors and dont want to mess the code up.

--- api/models/orders.py
+++ api/models/orders.py
@@ -12,5 +12,4 @@ class Order(Base):
     order_date = Column(DATETIME, nullable=False, server_default=str(datetime.now()))
     description = Column(String(300))
 
-    #coupon = relationship("Coupon", back_populates="order")
     order_details = relationship("OrderDetail", back_populates="order")
